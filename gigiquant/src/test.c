dc7e703f500c1c506a56c1eedbe9a6ee7b1932d6
Bostan Claudiu <claudiu.bostan@stud.acs.upb.ro>
2026-04-05 18:54:07 +0300

    tip: Testare Checker

---
+++ gigiquant/src/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main(int argc, char* argv[]) {
+   FILE *fi= fopen(argv[1], "r");
+   FILE *fo= fopen(argv[2], "w");;
+   fprintf(fo, "DEMO\n");
+   fclose(fi);
+   fclose(fo);
+   return 0;
+}
