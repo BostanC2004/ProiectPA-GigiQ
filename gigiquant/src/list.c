ec393f22749bc1f41f7eecd666db4fb9a149f4de
Bostan Claudiu <claudiu.bostan@stud.acs.upb.ro>
2026-04-24 16:58:18 +0300

    fix: Update la fisierele pt liste pt taskul 2

--- gigiquant/src/list.c
+++ gigiquant/src/list.c
@@ -1,48 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "list.h"
 
 Node *createNode(double val) {
     Node *newNode = (Node *)malloc(sizeof(Node));
-    if (newNode == NULL) {
-        return NULL;
-    }
+    if (newNode == NULL) return NULL;
     newNode->val = val;
     newNode->next = NULL;
     return newNode;
 }
 
+// Adaugare la final (folosita la Task 1)
 void addAtEnd(Node **head, double val) {
     Node *newNode = createNode(val);
-    if (newNode == NULL) {
-        exit(1);
-    }
-
     if (*head == NULL) {
         *head = newNode;
         return;
     }
-
     Node *aux = *head;
-    while (aux->next != NULL) {
-        aux = aux->next;
-    }
+    while (aux->next != NULL) aux = aux->next;
     aux->next = newNode;
 }
 
-int getLength(Node *head) {
-    int len = 0;
-    while (head != NULL) {
-        len++;
-        head = head->next;
-    }
-    return len;
+// Adaugare la inceput (Echivalent cu PUSH pentru stiva - util la Task 2)
+void addAtBeginning(Node **head, double val) {
+    Node *newNode = createNode(val);
+    newNode->next = *head;
+    *head = newNode;
 }
 
 void freeList(Node **head) {
     Node *curr = *head;
     while (curr != NULL) {
         Node *next = curr->next;
+        free(curr);
+        curr = next;
+    }
+    *head = NULL;
+}
+
+// Elibereaza toata structura de date (Actiuni + Listele lor de preturi)
+void freeStocks(Stock **head) {
+    Stock *curr = *head;
+    while (curr != NULL) {
+        Stock *next = curr->next;
+        freeList(&(curr->prices)); // Eliberam preturile orasului curent
         free(curr);
         curr = next;
     }
